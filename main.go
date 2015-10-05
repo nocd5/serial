0d078f3a91b30d42e56d3da20f74e16726a165e2
nocd5 <nocd5rd@gmail.com>
2015-10-05 21:31:00 +0900

    Add DataBytes, ParityMode and  StopBits options
    change serial library to jacobsa/go-serial/serial from tarm/serial

--- main.go
+++ main.go
@@ -6,13 +6,18 @@ import (
 	"log"
 	"os"
 
+	"github.com/jacobsa/go-serial/serial"
 	"github.com/jessevdk/go-flags"
-	"github.com/tarm/serial"
 )
 
 type Options struct {
-	Port string `short:"p" long:"port" description:"Serial Port" required:"true"`
-	Baud int    `short:"b" long:"baud" description:"Baud Rate" required:"true"`
+	// required
+	PortName string `short:"p" long:"port" description:"Serial Port" required:"true"`
+	BaudRate uint   `short:"b" long:"baud" description:"Baud Rate" required:"true"`
+	// optional
+	DataBits   uint   `long:"data" description:"Number of Data Bits" default:"8"`
+	ParityMode string `long:"parity" description:"Parity Mode. none/even/odd" default:"none"`
+	StopBits   uint   `long:"stop" description:"Number of Stop Bits" default:"1"`
 }
 
 var opts Options
@@ -23,8 +28,29 @@ func main() {
 		os.Exit(1)
 	}
 
-	config := &serial.Config{Name: opts.Port, Baud: opts.Baud}
-	port, err := serial.OpenPort(config)
+	var parityMode serial.ParityMode
+	switch opts.ParityMode {
+	case "none":
+		parityMode = serial.PARITY_NONE
+	case "odd":
+		parityMode = serial.PARITY_ODD
+	case "even":
+		parityMode = serial.PARITY_EVEN
+	default:
+		fmt.Fprintf(os.Stderr, "Invalid ParityMode: %s\n", opts.ParityMode)
+		fmt.Fprintf(os.Stderr, "`--parity` should be any one of none/odd/even\n")
+		os.Exit(1)
+	}
+
+	options := serial.OpenOptions{
+		PortName:   opts.PortName,
+		BaudRate:   opts.BaudRate,
+		DataBits:   opts.DataBits,
+		ParityMode: parityMode,
+		StopBits:   opts.StopBits,
+	}
+
+	port, err := serial.Open(options)
 	defer port.Close()
 	if err != nil {
 		log.Fatal(err)
